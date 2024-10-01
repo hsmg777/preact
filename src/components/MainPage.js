75efce5886eb08a7a7d9f0895f6da750a95ed126
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-10-01 14:42:18 -0500

    css aplicado a MainPage

---
+++ src/components/MainPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Si usas react-router-dom
+import '../components/styles/MainPage.css'
+
+const MainPage = () => {
+    return (
+        <div className='main-page'>
+            <div className='encabezado'>
+                <h1>PROYECTO CRUD USER</h1>
+            </div>
+            <div className='cuerpo'>
+                <div className='form'>
+                    <h2>BIENVENIDO</h2>
+                    <h4>Ingrese su usuario</h4>
+                    <input type='text'></input>
+                    <h4>Ingrese su contraseña</h4>
+                    <input type='password'></input>
+                    <button type="submit" className='reservar-button'>Acceder</button>
+                    <p>No tienes tu usuario? <Link to="/register">Registrate</Link> </p> {/* Usando Link */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
