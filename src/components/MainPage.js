13d73de2c6ea2e427bce7795440ca85de63be499
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-10-31 13:58:58 -0500

    new url api

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -10,7 +10,7 @@ const MainPage = () => {
     const history = useHistory();
 
     // URL base de la API expuesta por Ngrok
-    const BASE_URL = "http://127.0.0.1:5000/api/usuario";
+    const BASE_URL = "https://0e7e-2800-bf0-165-1282-999a-18a5-6b53-69a3.ngrok-free.app/api/usuario";
 
     const handleLogin = async (e) => {
         e.preventDefault();
--- src/components/Register.js
+++ src/components/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
     const history = useHistory();
 
     // URL base de la API expuesta por Ngrok
-    const BASE_URL = "https://10b3-2800-bf0-165-1282-999a-18a5-6b53-69a3.ngrok-free.app/api/usuario";
+    const BASE_URL = "https://0e7e-2800-bf0-165-1282-999a-18a5-6b53-69a3.ngrok-free.app/api/usuario";
 
     const volver = async () => {
         history.push({
